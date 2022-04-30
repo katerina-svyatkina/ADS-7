a13788f6173abac6a0f029598e1b701c6e9d8961
Екатерина Святкина <99070196+katerina-svyatkina@users.noreply.github.com>
2022-04-30 17:49:09 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,82 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+    struct ITEM {
+        T value;
+        ITEM* next;
+        ITEM* prev;
+    };
+    ITEM* head, * tail, * current;
+    TPQueue::ITEM* create(const T& value) {
+        ITEM* item = new ITEM;
+        //(*item).value = value;
+        item->value = value;
+        item->next = nullptr;
+        item->prev = nullptr;
+        return item;
+    }
+    T rmHead() {
+        if (head) {
+            ITEM* temp = head->next;
+            if (temp)
+                temp->prev = nullptr;
+            T value = head->value;
+            delete head;
+            head = temp;
+            return value;
+        }
+        else {
+            throw std::string("Empty!");
+        }
+    }
+public:
+    TPQueue() : head(nullptr), tail(nullptr), current(nullptr) {}
+    TPQueue(const T& value) {
+        head = tail = create(value);
+        current = nullptr;
+    }
+    ~TPQueue() {
+        while (head)
+            rmHead();
+    }
+    bool isEmpty() const {
+        return !head;
+    }
+    void print() const {
+        ITEM* temp = head;
+        while (temp) {
+            std::cout << temp->value.ch << " "<<temp->value.prior<<std::endl;
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+
+    void push(const T& value) {
+        ITEM* temp = create(value);
+        if (isEmpty()) {
+            head = tail = temp;
+        }
+        else {
+            current = tail;
+            while (current->prev && current->value.prior < value.prior) {
+                current = current->prev;
+            }
+            ITEM* tmp_prev = current->prev;
+            current->prev = temp;
+            temp->next = current;
+            if (tmp_prev) {
+                temp->prev = tmp_prev;
+                tmp_prev->next = temp;
+            }
+            else {
+                head = temp;
+            }
+        }
+    }
+    T pop() {
+        return rmHead();
+    }
 };
 
 struct SYM {
